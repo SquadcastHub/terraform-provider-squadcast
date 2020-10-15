5d67e59319f35cb557ed08ec82111aa09f952d6f
roshan.r <roshan.aloor@gmail.com>
2020-10-15 19:49:36 +0530

    Deleted github workflow actions

--- examples/main.tf
+++ examples/main.tf
@@ -10,7 +10,7 @@ terraform {
 
 
 provider "squadcast" {
-  squadcast_token = "2287c30509d3c976e0a398f89325392e959dcb1c02432e1ca80987927832830034e442b3cce9c1e118fed3f897439c877cb253cc7dd89d085b9c8fd15a8fe8d1"
+  squadcast_token = "YOUR_SQUADCAST_TOKEN"
 }
 
 data "squadcast_escalation_policy" "rozd" {
