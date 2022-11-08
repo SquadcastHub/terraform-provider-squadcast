da90fd25bf4d4a6227b22cbaf053d6b5aa17ffe5
Mohammad Muazam <mohammad@squadcast.com>
2022-11-08 18:34:19 +0530

    fix: updated logic for data source

--- internal/provider/data_source_team_role.go
+++ internal/provider/data_source_team_role.go
@@ -19,105 +19,33 @@ func dataSourceTeamRole() *schema.Resource {
 
 		Schema: map[string]*schema.Schema{
 			"id": {
-				Description: "User id.",
+				Description: "Role id.",
 				Type:        schema.TypeString,
 				Computed:    true,
 			},
-			"first_name": {
-				Description: "User first name.",
-				Type:        schema.TypeString,
-				Computed:    true,
-			},
-			"last_name": {
-				Description: "User last name.",
-				Type:        schema.TypeString,
-				Computed:    true,
-			},
-			"name": {
-				Description: "User name, automatically computed from first name and last name.",
-				Type:        schema.TypeString,
-				Computed:    true,
-			},
-			"email": {
-				Description:  "User email.",
+			"team_id": {
+				Description:  "Team id.",
 				Type:         schema.TypeString,
 				Required:     true,
-				ValidateFunc: validation.StringIsNotWhiteSpace,
+				ValidateFunc: tf.ValidateObjectID,
+				ForceNew:     true,
 			},
-			"is_email_verified": {
-				Description: "Denotes if the user has verified their email or not.",
+			"name": {
+				Description:  "TeamRole name.",
+				Type:         schema.TypeString,
+				Required:     true,
+				ValidateFunc: validation.StringLenBetween(1, 255),
+			},
+			"default": {
+				Description: "Role is default?.",
 				Type:        schema.TypeBool,
-				Computed:    true,
-			},
-			"phone": {
-				Description: "User phone number.",
-				Type:        schema.TypeString,
-				Computed:    true,
-			},
-			"is_phone_verified": {
-				Description: "Denotes if the user has verified their phone number or not.",
-				Type:        schema.TypeBool,
-				Computed:    true,
-			},
-			"is_override_dnd_enabled": {
-				Description: "Deprecated, this can be ignored.",
-				Type:        schema.TypeBool,
-				Computed:    true,
-			},
-			"role": {
-				Description: "User role.",
-				Type:        schema.TypeString,
-				Computed:    true,
-			},
-			"time_zone": {
-				Description: "User time_zone.",
-				Type:        schema.TypeString,
 				Computed:    true,
 			},
 			"abilities": {
-				Description: "Denotes the Permissions / abilities of the user.",
-				Type:        schema.TypeList,
-				Computed:    true,
+				Type:     schema.TypeList,
+				Computed: true,
 				Elem: &schema.Schema{
 					Type: schema.TypeString,
-				},
-			},
-			"notification_rules": {
-				Description: "User Personal Notification Rules.",
-				Type:        schema.TypeList,
-				Computed:    true,
-				Elem: &schema.Resource{
-					Schema: map[string]*schema.Schema{
-						"type": {
-							Description: "Personal notification rule type.",
-							Type:        schema.TypeString,
-							Computed:    true,
-						},
-						"delay_minutes": {
-							Description: "notification rule delay_minutes, (to be deprecated).",
-							Type:        schema.TypeInt,
-							Computed:    true,
-						},
-					},
-				},
-			},
-			"oncall_reminder_rules": {
-				Description: "User's personal on-call reminder notification rules.",
-				Type:        schema.TypeList,
-				Computed:    true,
-				Elem: &schema.Resource{
-					Schema: map[string]*schema.Schema{
-						"type": {
-							Description: "oncall reminder rule type.",
-							Type:        schema.TypeString,
-							Computed:    true,
-						},
-						"delay_minutes": {
-							Description: "oncall reminder rule delay_minutes.",
-							Type:        schema.TypeInt,
-							Computed:    true,
-						},
-					},
 				},
 			},
 		},
@@ -127,17 +55,19 @@ func dataSourceTeamRole() *schema.Resource {
 func dataSourceTeamRoleRead(ctx context.Context, d *schema.ResourceData, meta any) diag.Diagnostics {
 	client := meta.(*api.Client)
 
-	email := d.Get("email").(string)
+	teamRoleName := d.Get("name").(string)
+	team_id := d.Get("team_id").(string)
 
-	tflog.Info(ctx, "Reading user", tf.M{
-		"email": email,
+	tflog.Info(ctx, "Reading team_role", tf.M{
+		"name": teamRoleName,
+		"id":  team_id,
 	})
-	user, err := client.GetUserByEmail(ctx, email)
+	teamRole, err := client.GetTeamRoleByName(ctx, team_id, teamRoleName)
 	if err != nil {
 		return diag.FromErr(err)
 	}
 
-	if err = tf.EncodeAndSet(user, d); err != nil {
+	if err = tf.EncodeAndSet(teamRole, d); err != nil {
 		return diag.FromErr(err)
 	}
 
