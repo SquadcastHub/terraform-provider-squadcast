959e54755326b553639af1b4f416eec147b4adb8
Mohammad Muazam <mohammad@squadcast.com>
2023-07-05 12:22:39 +0530

    updates test for new resources

--- internal/provider/resource_schedule_v2_test.go
+++ internal/provider/resource_schedule_v2_test.go
@@ -16,7 +16,7 @@ func TestAccResourceScheduleV2(t *testing.T) {
 
 	resourceName := "squadcast_schedule_v2.test"
 	resource.UnitTest(t, resource.TestCase{
-		// PreCheck:          func() { testAccPreCheck(t) },
+		PreCheck:          func() { testAccPreCheck(t) },
 		ProviderFactories: providerFactories,
 		CheckDestroy:      testAccCheckScheduleV2Destroy,
 		Steps: []resource.TestStep{
@@ -32,16 +32,6 @@ func TestAccResourceScheduleV2(t *testing.T) {
 					resource.TestCheckResourceAttr(resourceName, "entity_owner.0.id", "613611c1eb22db455cfa789f"),
 					resource.TestCheckResourceAttr(resourceName, "tags.0.key", "key1"),
 					resource.TestCheckResourceAttr(resourceName, "tags.0.value", "value1"),
-					resource.TestCheckResourceAttr(resourceName, "tags.0.color", "#9900ef"),
-					resource.TestCheckResourceAttr(resourceName, "rotation.0.name", "Test Rotation"),
-					resource.TestCheckResourceAttr(resourceName, "rotation.0.start_date", "2023-06-09T00:00:00Z"),
-					resource.TestCheckResourceAttr(resourceName, "rotation.0.period", "custom"),
-					resource.TestCheckResourceAttr(resourceName, "rotation.0.change_participants_frequency", "1"),
-					resource.TestCheckResourceAttr(resourceName, "rotation.0.change_participants_unit", "week"),
-					resource.TestCheckResourceAttr(resourceName, "rotation.0.custom_period_frequency", "1"),
-					resource.TestCheckResourceAttr(resourceName, "rotation.0.custom_period_unit", "week"),
-					resource.TestCheckResourceAttr(resourceName, "rotation.0.participant_groups.0.participants.0.type", "user"),
-					resource.TestCheckResourceAttr(resourceName, "rotation.0.participant_groups.0.participants.0.id", "613611c1eb22db455cfa789f"),
 				),
 			},
 			{
@@ -56,16 +46,6 @@ func TestAccResourceScheduleV2(t *testing.T) {
 					resource.TestCheckResourceAttr(resourceName, "entity_owner.0.id", "613611c1eb22db455cfa789f"),
 					resource.TestCheckResourceAttr(resourceName, "tags.0.key", "key1"),
 					resource.TestCheckResourceAttr(resourceName, "tags.0.value", "value1"),
-					resource.TestCheckResourceAttr(resourceName, "tags.0.color", "#9900ef"),
-					resource.TestCheckResourceAttr(resourceName, "rotation.0.name", "Test Rotation"),
-					resource.TestCheckResourceAttr(resourceName, "rotation.0.start_date", "2023-06-09T00:00:00Z"),
-					resource.TestCheckResourceAttr(resourceName, "rotation.0.period", "custom"),
-					resource.TestCheckResourceAttr(resourceName, "rotation.0.change_participants_frequency", "1"),
-					resource.TestCheckResourceAttr(resourceName, "rotation.0.change_participants_unit", "week"),
-					resource.TestCheckResourceAttr(resourceName, "rotation.0.custom_period_frequency", "1"),
-					resource.TestCheckResourceAttr(resourceName, "rotation.0.custom_period_unit", "week"),
-					resource.TestCheckResourceAttr(resourceName, "rotation.0.participant_groups.0.participants.0.type", "user"),
-					resource.TestCheckResourceAttr(resourceName, "rotation.0.participant_groups.0.participants.0.id", "613611c1eb22db455cfa789f"),
 				),
 			},
 			{
@@ -113,22 +93,7 @@ func testAccResourceScheduleV2Config(scheduleName string) string {
 			tags {
 				key = "key1"
 				value = "value1"
-				color = "#9900ef"
 			}
-			rotation {
-				name = "Test Rotation"
-				start_date = "2023-06-09T00:00:00Z"
-				period = "custom"
-				change_participants_frequency = 1
-				change_participants_unit = "week"
-				custom_period_frequency = 1
-				custom_period_unit = "week"
-				participant_groups {
-					participants {
-						type = "user"
-						id = "613611c1eb22db455cfa789f"
-					}
-				}
 		}
 	`, scheduleName)
 }
@@ -147,22 +112,7 @@ func testAccResourceScheduleV2Config_update(scheduleName string) string {
 			tags {
 				key = "key1"
 				value = "value1"
-				color = "#9900ef"
 			}
-			rotation {
-				name = "Test Rotation"
-				start_date = "2023-06-09T00:00:00Z"
-				period = "custom"
-				change_participants_frequency = 1
-				change_participants_unit = "week"
-				custom_period_frequency = 1
-				custom_period_unit = "week"
-				participant_groups {
-					participants {
-						type = "user"
-						id = "613611c1eb22db455cfa789f"
-					}
-				}
 		}
 	`, scheduleName)
 }
