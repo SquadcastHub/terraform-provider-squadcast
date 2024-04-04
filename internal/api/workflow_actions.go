d6f0a6683289be07393c74173b754cc82760873e
Roshan shetty <roshan.aloor@gmail.com>
2024-04-04 20:32:54 +0530

    support slo action

--- internal/api/workflow_actions.go
+++ internal/api/workflow_actions.go
@@ -8,6 +8,7 @@ import (
 	"github.com/squadcast/terraform-provider-squadcast/internal/tf"
 )
 
+// WorkflowAction represents a workflow action request & terraform resource
 type WorkflowAction struct {
 	ID         int                `json:"id" tf:"id"`
 	Name       string             `json:"name" tf:"name"`
@@ -18,26 +19,33 @@ type WorkflowAction struct {
 type WorkflowActionData struct {
 	Note     string   `json:"note" tf:"note"`
 	Runbooks []string `json:"runbooks" tf:"runbooks"`
+	SLO      int      `json:"slo" tf:"slo"`
+	SLIs     []string `json:"slis" tf:"slis"`
 }
 
-// Runbook action
-type WorkflowActionRunbookRes struct {
-	ID         int                            `json:"id" tf:"id"`
-	Name       string                         `json:"name" tf:"name"`
-	WorkflowID int                            `json:"workflow_id" tf:"workflow_id"`
-	Data       []WorkflowActionRunbookDataRes `json:"data" tf:"-"`
+// WorkflowActionRes represents a workflow action response
+type WorkflowActionRes struct {
+	ID         int                   `json:"id" tf:"id"`
+	Name       string                `json:"name" tf:"name"`
+	WorkflowID int                   `json:"workflow_id" tf:"workflow_id"`
+	Data       WorkflowActionDataRes `json:"data" tf:"-"`
 }
 
-type WorkflowActionRunbookDataRes struct {
-	ID   string `json:"id" tf:"id"`
-	Name string `json:"name" tf:"name"`
+type WorkflowActionDataRes struct {
+	Note     string   `json:"note" tf:"note"`
+	SLO      string   `json:"name" tf:"slo"` // response sends id where request sends name
+	SLIs     []string `json:"slis" tf:"slis"`
+	Runbooks []struct {
+		ID   string `json:"id"`
+		Name string `json:"name"`
+	} `json:"runbooks"`
 }
 
 // TODO: Check if we even need this??
-func (twc *WorkflowActionData) Encode() (tf.M, error) {
+func (twc *WorkflowActionDataRes) Encode() (tf.M, error) {
 	return tf.Encode(twc)
 }
-func (w *WorkflowAction) Encode() (tf.M, error) {
+func (w *WorkflowActionRes) Encode() (tf.M, error) {
 	m, err := tf.Encode(w)
 	if err != nil {
 		return nil, err
@@ -47,52 +55,20 @@ func (w *WorkflowAction) Encode() (tf.M, error) {
 	return m, nil
 }
 
-// Runbook action response
-func (w *WorkflowActionRunbookRes) Encode() (tf.M, error) {
-	m, err := tf.Encode(w)
-	if err != nil {
-		return nil, err
-	}
-
-	m["workflow_id"] = fmt.Sprintf("%d", w.WorkflowID)
-	return m, nil
-}
-
-func (twc *WorkflowActionRunbookDataRes) Encode() (tf.M, error) {
-	return tf.Encode(twc)
-}
-
-func (client *Client) CreateWorkflowAction(ctx context.Context, workflowID string, workflowAction *WorkflowAction) (*WorkflowAction, error) {
-	url := fmt.Sprintf("%s/workflows/%s/actions", client.BaseURLV3, workflowID)
+func (client *Client) CreateWorkflowAction(ctx context.Context, workflowID string, workflowAction *WorkflowAction) (*WorkflowActionRes, error) {
 	// url := "https://webhook.site/3ee9072d-5587-4bb3-95af-bed7db01fffa"
-	return Request[WorkflowAction, WorkflowAction](http.MethodPost, url, client, ctx, workflowAction)
-}
-
-func (client *Client) GetWorkflowActionById(ctx context.Context, workflowID, actionID string) (*WorkflowAction, error) {
-	url := fmt.Sprintf("%s/workflows/%s/actions/%s", client.BaseURLV3, workflowID, actionID)
-	return Request[WorkflowAction, WorkflowAction](http.MethodGet, url, client, ctx, nil)
-}
-
-func (client *Client) UpdateWorkflowAction(ctx context.Context, workflowID, actionID string, workflowAction *WorkflowAction) (*WorkflowAction, error) {
-	url := fmt.Sprintf("%s/workflows/%s/actions/%s", client.BaseURLV3, workflowID, actionID)
-	return Request[WorkflowAction, WorkflowAction](http.MethodPatch, url, client, ctx, workflowAction)
-}
-
-// Runbook action
-func (client *Client) CreateRunbookWorkflowAction(ctx context.Context, workflowID string, workflowAction *WorkflowAction) (*WorkflowActionRunbookRes, error) {
 	url := fmt.Sprintf("%s/workflows/%s/actions", client.BaseURLV3, workflowID)
-	// url := "https://webhook.site/3ee9072d-5587-4bb3-95af-bed7db01fffa"
-	return Request[WorkflowAction, WorkflowActionRunbookRes](http.MethodPost, url, client, ctx, workflowAction)
+	return Request[WorkflowAction, WorkflowActionRes](http.MethodPost, url, client, ctx, workflowAction)
 }
 
-func (client *Client) GetRunbookWorkflowActionById(ctx context.Context, workflowID, actionID string) (*WorkflowActionRunbookRes, error) {
+func (client *Client) GetWorkflowActionById(ctx context.Context, workflowID, actionID string) (*WorkflowActionRes, error) {
 	url := fmt.Sprintf("%s/workflows/%s/actions/%s", client.BaseURLV3, workflowID, actionID)
-	return Request[WorkflowAction, WorkflowActionRunbookRes](http.MethodGet, url, client, ctx, nil)
+	return Request[WorkflowAction, WorkflowActionRes](http.MethodGet, url, client, ctx, nil)
 }
 
-func (client *Client) UpdateRunbookWorkflowAction(ctx context.Context, workflowID, actionID string, workflowAction *WorkflowAction) (*WorkflowActionRunbookRes, error) {
+func (client *Client) UpdateWorkflowAction(ctx context.Context, workflowID, actionID string, workflowAction *WorkflowAction) (*WorkflowActionRes, error) {
 	url := fmt.Sprintf("%s/workflows/%s/actions/%s", client.BaseURLV3, workflowID, actionID)
-	return Request[WorkflowAction, WorkflowActionRunbookRes](http.MethodPatch, url, client, ctx, workflowAction)
+	return Request[WorkflowAction, WorkflowActionRes](http.MethodPatch, url, client, ctx, workflowAction)
 }
 
 func (client *Client) DeleteWorkflowAction(ctx context.Context, workflowID, actionID string) (*any, error) {
--- internal/provider/resource_workflow_action.go
+++ internal/provider/resource_workflow_action.go
@@ -28,7 +28,7 @@ func resourceWorkflowAction() *schema.Resource {
 				Type:         schema.TypeString,
 				Description:  "The name of the action",
 				Required:     true,
-				ValidateFunc: validation.StringInSlice([]string{"sq_add_incident_note", "sq_attach_runbooks"}, false),
+				ValidateFunc: validation.StringInSlice([]string{"sq_add_incident_note", "sq_attach_runbooks", "sq_mark_incident_slo_affecting"}, false),
 			},
 			"note": {
 				Type:        schema.TypeString,
@@ -38,6 +38,19 @@ func resourceWorkflowAction() *schema.Resource {
 			"runbooks": {
 				Type:        schema.TypeList,
 				Description: "The runbooks to be added to the incident",
+				Optional:    true,
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+			},
+			"slo": {
+				Type:        schema.TypeInt,
+				Description: "ID of the SLO to be added to the incident",
+				Optional:    true,
+			},
+			"slis": {
+				Type:        schema.TypeList,
+				Description: "The SLIs to be added to the incident",
 				Optional:    true,
 				Elem: &schema.Schema{
 					Type: schema.TypeString,
@@ -62,63 +75,23 @@ func resourceWorkflowActionCreate(ctx context.Context, d *schema.ResourceData, m
 		Name: d.Get("name").(string),
 		Data: api.WorkflowActionData{
 			Note:     d.Get("note").(string),
+			SLO:      d.Get("slo").(int),
+			SLIs:     tf.ListToSlice[string](d.Get("slis")),
 			Runbooks: runbooks,
 		},
 	}
 
 	workflowID := d.Get("workflow_id").(string)
 
-	switch d.Get("name").(string) {
-	case "sq_add_incident_note":
-		workflowActionResponse, err := client.CreateWorkflowAction(ctx, workflowID, workflowAction)
-		if err != nil {
-			return diag.FromErr(err)
-		}
-		workflowActionID := strconv.FormatUint(uint64(workflowActionResponse.ID), 10)
-		d.SetId(workflowActionID)
-	case "sq_attach_runbooks":
-		workflowActionResponse, err := client.CreateRunbookWorkflowAction(ctx, workflowID, workflowAction)
-		if err != nil {
-			return diag.FromErr(err)
-		}
-		workflowActionID := strconv.FormatUint(uint64(workflowActionResponse.ID), 10)
-		d.SetId(workflowActionID)
+	workflowActionResponse, err := client.CreateWorkflowAction(ctx, workflowID, workflowAction)
+	if err != nil {
+		return diag.FromErr(err)
 	}
+
+	workflowActionID := strconv.FormatUint(uint64(workflowActionResponse.ID), 10)
+	d.SetId(workflowActionID)
 
 	return resourceWorkflowActionRead(ctx, d, meta)
-}
-
-func resourceWorkflowActionRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	client := meta.(*api.Client)
-
-	tflog.Info(ctx, "Reading workflow action", tf.M{
-		"name":        d.Get("name").(string),
-		"action_id":   d.Id(),
-		"worfklow_id": d.Get("workflow_id").(string),
-	})
-
-	workflowID := d.Get("workflow_id").(string)
-	workflowActionID := d.Id()
-
-	switch d.Get("name").(string) {
-	case "sq_add_incident_note":
-		workflowAction, err := client.GetWorkflowActionById(ctx, workflowID, workflowActionID)
-		if err != nil {
-			return diag.FromErr(err)
-		}
-		if err = tf.EncodeAndSet(workflowAction, d); err != nil {
-			return diag.FromErr(err)
-		}
-	case "sq_attach_runbooks":
-		workflowAction, err := client.GetRunbookWorkflowActionById(ctx, workflowID, workflowActionID)
-		if err != nil {
-			return diag.FromErr(err)
-		}
-		if err = tf.EncodeAndSet(workflowAction, d); err != nil {
-			return diag.FromErr(err)
-		}
-	}
-	return nil
 }
 
 func resourceWorkflowActionUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
@@ -136,26 +109,44 @@ func resourceWorkflowActionUpdate(ctx context.Context, d *schema.ResourceData, m
 		Name: d.Get("name").(string),
 		Data: api.WorkflowActionData{
 			Note:     d.Get("note").(string),
+			SLO:      d.Get("slo").(int),
+			SLIs:     tf.ListToSlice[string](d.Get("slis")),
 			Runbooks: runbooks,
 		},
 	}
 
 	workflowID := d.Get("workflow_id").(string)
 
-	switch d.Get("name").(string) {
-	case "sq_add_incident_note":
-		_, err := client.UpdateWorkflowAction(ctx, workflowID, d.Id(), workflowAction)
-		if err != nil {
-			return diag.FromErr(err)
-		}
-	case "sq_attach_runbooks":
-		_, err := client.UpdateRunbookWorkflowAction(ctx, workflowID, d.Id(), workflowAction)
-		if err != nil {
-			return diag.FromErr(err)
-		}
+	_, err := client.UpdateWorkflowAction(ctx, workflowID, d.Id(), workflowAction)
+	if err != nil {
+		return diag.FromErr(err)
 	}
 
 	return resourceWorkflowActionRead(ctx, d, meta)
+}
+
+func resourceWorkflowActionRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	client := meta.(*api.Client)
+
+	tflog.Info(ctx, "Reading workflow action", tf.M{
+		"name":        d.Get("name").(string),
+		"action_id":   d.Id(),
+		"worfklow_id": d.Get("workflow_id").(string),
+	})
+
+	workflowID := d.Get("workflow_id").(string)
+	workflowActionID := d.Id()
+
+	workflowAction, err := client.GetWorkflowActionById(ctx, workflowID, workflowActionID)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	if err = tf.EncodeAndSet(workflowAction, d); err != nil {
+		return diag.FromErr(err)
+	}
+
+	return nil
 }
 
 func resourceWorkflowActionDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
