ec2c19b978833385fd57296a7760b5828e3f0876
Mohammad Muazam <110392771+mohammadmuazam@users.noreply.github.com>
2024-09-18 14:07:41 +0530

    Revert "Deprecate `resource_user`"

--- internal/provider/resource_user.go
+++ internal/provider/resource_user.go
@@ -13,8 +13,7 @@ import (
 
 func resourceUser() *schema.Resource {
 	return &schema.Resource{
-		Description:        "User resource.",
-		DeprecationMessage: "[DEPRECATED] This resource is deprecated, please use `squadcast_user_membership` instead. We are introducing invite as an additional step in the user onboarding flow to enhance security. The new resource will be used to invite users to an organization.",
+		Description: "User resource.",
 
 		CreateContext: resourceUserCreate,
 		ReadContext:   resourceUserRead,
